60e0ddcbe9bc376896d0e3c3a5132565d1acd315
JorMaister <jorgejjs86@gmail.com>
2022-01-12 11:14:46 +0100

    Remove old code from tests init

--- pyseg/tests/__init__.py
+++ pyseg/tests/__init__.py
@@ -1,19 +1,5 @@
 from pyworkflow.tests import DataSet
 
-# DataSet(name='pyseg', folder='pyseg',
-#         files={'presegTomo': 'Pertuzumab_1_defocus_25um_tomo_7_aliSIRT_EED.mrc',
-#                'presegMask': 'Pertuzumab_1_defocus_25um_tomo_7_aliSIRT_EED_material.mrc',
-#                'posRecMask': 'cylMaskk40.mrc',
-#                'mbMask': 'mbMask40.mrc',
-#                'subtomosTbl': 'Pertuzumab_1_defocus_25um_tomo_7_aliSIRT_EED.tbl',
-#                'coordsTbl': 'Pertuzumab_1_defocus_25um_tomo_7_aliSIRT_EED.tbl',
-#                'posRecCoordsDir': 'posRecCoords',
-#                'coords': 'posRecCoords/*.mrc',
-#                'tiltSeries': 'Pertuzumab_1_defocus_25um_tomo_7_ali.mrc',
-#                'tltFile': 'Pertuzumab_1_defocus_25um_tomo_7_ali.tlt',
-#                'doseFile': 'Pertuzumab_1_defocus_25um_tomo_7_ali_ExpDose.txt'
-#         })
-
 DataSet(name='emd_10439', folder='emd_10439',
         files={
                'tomoEmd10439': 'tomograms/emd_10439.mrc',
